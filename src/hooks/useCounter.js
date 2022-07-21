39b3ec62d88865d1d7cce290a8f78eb38e88b3bd
JohnPosada <johnposada2000@gmail.com>
2022-07-21 17:50:09 -0500

    Se corrigio el hook useCounter y el componente todoItem

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -6,11 +6,11 @@ export const useCounter = (value = 1) => {
     const [counter, setCounter] = useState(value);
 
     const increment = ()=> {
-        setCounter(counter+1)
+        setCounter((counter) => counter+1)
     }
     const decrement = ()=> {
-        if (counter === 1) return
-        setCounter(counter-1)
+        if (counter === 0) return
+        setCounter((counter) => counter-1)
     }
     const reset = ()=> {
         setCounter(value)
