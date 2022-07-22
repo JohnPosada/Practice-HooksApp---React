134a18bc0eaec11e12b0958e8cf0c718aba9ae7a
JohnPosada <johnposada2000@gmail.com>
2022-07-21 19:37:18 -0500

    se actualizo el archivo main.jsx y se agregaron algunos estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HooksApp } from './HooksApp'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter } from "react-router-dom";
+
+import { MainApp } from './useContext/MainApp';
+//import { TodoApp } from './useReducer/TodoApp'
+//import './useReducer/introReducer.js'
+//import { Padre } from './tareaMemo/Padre'
+//import { CallbackHook } from './memo/CallbackHook'
+//import { Memorize } from './memo/Memorize'
+//import { FocusScreen } from './useRef/FocusScreen'
+//import { MultipleCustomHook } from './Example/MultipleCustomHook'
+//import { HooksApp } from './HooksApp'
+//import { SimpleForm } from './useEffect/SimpleForm'
+//import { CounterApp } from './UseState/CounterApp'
+//import { CounterAppWithHooks } from './UseState/CounterAppWithHooks'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HooksApp />
-  </React.StrictMode>
+  
+  <BrowserRouter>
+    <React.StrictMode>
+      <MainApp />
+    </React.StrictMode>
+  </BrowserRouter>
 )
